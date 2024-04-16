77a2188e0a6b607265a69f4baee01b1a2562e571
илья бугай <bugai.samara@yandex.ru>
2024-04-16 16:56:27 +0400

    Урок 1. Введение в классы. Поля класса. Процедурное программирование, ООП, организация кода

---
+++ src/Task1/Department.java
@@ -0,0 +1,6 @@
+package Task1;
+
+public class Faculty {
+    String name;
+    String dean;
+}
---
+++ src/Task1/Main.java
@@ -0,0 +1,10 @@
+import Task1.Faculty;
+import Task1.University;
+
+public class Main {
+    public static void main(String[] args) {
+        Faculty faculty = new Faculty();
+        String name = faculty.name;
+
+    }
+}
